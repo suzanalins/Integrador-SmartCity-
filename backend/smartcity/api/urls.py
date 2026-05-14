e948576053a4607ea4163d587883f59591f0165a
Suzana Lins <suzanalins488@gmail.com>
2026-05-13 23:05:55 -0300

    iniciando o front

--- backend/smartcity/api/urls.py
+++ backend/smartcity/api/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
+ 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-
+ 
 from api.views import *
-
-
+ 
+ 
 #aqui ta definindo as rotas da api
 router = DefaultRouter()
 router.register(r'locais', LocaisViewSet)
@@ -18,17 +18,25 @@ router.register(r'ambientes', AmbienteViewSet)
 router.register(r'microcontroladores', MicrocontroladoresViewSet)
 router.register(r'sensores', SensorViewSet)
 router.register(r'historicos', HistoricoViewSet)
-
+ 
 urlpatterns = [
     #pegando as rotas p autenticação JWT
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),#aqui vc coloca o username e a password e ele te da 2 tipos de token, op refresh e o acess
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'), #pra o refresh funcionar vc precisa passar o token de refresh no json
-
+ 
     #puxando o caminho p registrar um novo user
     path('register/', RegisterView.as_view(), name='register'),
-
+ 
     #puxando o caminho p pegar os dados do user já logado
     path('me/', UsuarioMeView.as_view(), name='me'),
+ 
+    #rotas das funções de importação por Excel registradas
+    path('popular/locais/', popular_locais, name='popular_locais'),
+    path('popular/responsaveis/', popular_responsaveis, name='popular_responsaveis'),
+    path('popular/ambientes/', popular_ambientes, name='popular_ambientes'),
+    path('popular/microcontroladores/', popular_microcontroladores, name='popular_microcontroladores'),
+    path('popular/sensores/', popular_sensores, name='popular_sensores'),
+    path('popular/historicos/', popular_historicos, name='popular_historicos'),
+ 
     path('', include(router.urls)),
 ]
-
